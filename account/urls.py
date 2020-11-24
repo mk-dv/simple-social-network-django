ce173ddd540b310e17ee2f4140a925581c18756f
Maxim Kalita <kalitamaxwork@gmail.com>
2020-11-24 14:08:31 +0300

    Remove commented routes, add comments for left routes

--- account/urls.py
+++ account/urls.py
@@ -3,51 +3,9 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    # path('login/', auth_views.LoginView.as_view(), name='login'),
-    # path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('', views.dashboard, name='dashboard'),
-    # An `auth_views.PasswordChangeView` validates the password change form,
-    # and uses the template `<app>/registration/password_change_form.html`.
-    # path(
-    #     'password_change/',
-    #     auth_views.PasswordChangeView.as_view(),
-    #     name='password_change'
-    # ),
-    # An `auth_views.PasswordChangeDoneView` displays a message about
-    # successful password change using the template
-    # `<app>/registration/password_change_done.html`.
-    # path(
-    #     'password_change/done',
-    #     auth_views.PasswordChangeDoneView.as_view(),
-    #     name='password_change_done'
-    # ),
-
-    # Password recovery paths.
-
-    # Sends an email only if the specified email belongs to one of the existing
-    # users.
-    # path(
-    #     'password_reset/',
-    #     auth_views.PasswordResetView.as_view(),
-    #     name='password_reset'
-    # ),
-    # path(
-    #     'password_reset_done/',
-    #     auth_views.PasswordResetDoneView.as_view(),
-    #     name='password_reset_done'
-    # ),
-    # PasswordResetView gets kwargs `uidb64` and `token`.
-    # path(
-    #     'reset/<uidb64>/<token>/',
-    #     auth_views.PasswordResetConfirmView.as_view(),
-    #     name='password_reset_confirm'
-    # ),
-    # path(
-    #     'reset/done/',
-    #     auth_views.PasswordResetCompleteView.as_view(),
-    #     name='password_reset_complete'
-    # ),
     path('', include('django.contrib.auth.urls')),
+    # The page for new user registration.
     path('register/', views.register, name='register'),
+    # The page for editing user profile data.
     path('edit/', views.edit, name='edit'),
 ]
