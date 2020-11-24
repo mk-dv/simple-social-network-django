fb56a4936559556e7e4163d0199ca02c727a8e67
Maxim Kalita <kalitamaxwork@gmail.com>
2020-11-24 14:33:38 +0300

    fix(user registration form) password repeat field

--- account/forms.py
+++ account/forms.py
@@ -49,5 +49,5 @@ class UserRegistrationForm(forms.ModelForm):
     def clean_password2(self):
         cd = self.cleaned_data
         if cd['password'] != cd['password2']:
-            raise form.ValidationError("Passwords don't match.")
+            raise forms.ValidationError("Passwords don't match.")
         return cd['password2']
