5ea163953735b8ff87e5d48c35877193a8b51c51
Maxim Kalita <kalitamaxwork@gmail.com>
2020-11-26 22:53:34 +0300

    refactor(account) remove unused custom user logging view

--- account/views.py
+++ account/views.py
@@ -34,34 +34,6 @@ def edit(request):
     )
 
 
-def user_login(request):
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        user = None
-        if form.is_valid():
-            cd = form.cleaned_data
-            user = authenticate(
-                request, username=cd['username'], password=cd['password']
-            )
-        if user is not None:
-            if user.is_active:
-                # Apparently, all the magic happens here, `login` from
-                # `django.contrib.auth` binds the user to the session.
-                # Saves the user’s ID in the session, using Django’s session
-                # framework(middleware?).
-                # Note that any data set during the anonymous session is
-                # retained in the session after a user logs in.
-                login(request, user)
-                return HttpResponse('Authenticated successfully')
-            else:
-                return HttpResponse('Disabled account')
-        else:
-            return HttpResponse('Invalid login or password')
-    else:
-        form = LoginForm()
-    return render(request, 'account/login.html', {'form': form})
-
-
 @login_required
 def dashboard(request):
     return render(request, 'account/dashboard.html', {'section': 'dashboard'})
