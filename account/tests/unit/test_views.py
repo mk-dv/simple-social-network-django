7795781b0e42bba5aa51ab71201eab3f33340c4d
Maxim Kalita <kalitamaxwork@gmail.com>
2020-12-27 08:43:00 +0300

    REBASE:небольшие изм в конфигах

---
+++ account/tests/unit/test_views.py
@@ -0,0 +1,12 @@
+import pytest
+from django.urls import reverse
+from django.conf import settings
+
+
+# Example
+# @pytest.mark.django_db
+# def test_edit(client):
+#     """ Make sure that we get valid JSON response if we have no users created """
+#     response = client.get(reverse('fetch-users'))
+#     data = response.json()
+#     assert {"users": []} == data
